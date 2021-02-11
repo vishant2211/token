f60c0148c4066198cfd74d853013b979851e67d7
vishant <vishant.dhandha@niyogin.in>
2021-02-11 23:58:34 +0530

    instrument path bug fixed

--- instruments/index.js
+++ instruments/index.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const fs = require('fs');
 const https = require('https');
 const path = require('path');
-const angelTokenPath = path.join('instruments', 'angelToken.js');
-const zerodhaTokenPath = path.join('instruments', 'zerodhaToken.js');
+const angelTokenPath = path.join(__dirname, 'angelToken.js');
+const zerodhaTokenPath = path.join(__dirname, 'zerodhaToken.js');
 
 axios({
     method: 'GET',
@@ -17,8 +17,8 @@ axios({
     if (data && data.data && Array.isArray(data.data) && data.data.length > 0) {
         try {
             let allSeg = ['NSE', 'BSE', 'NFO', 'CDS', 'MCX', 'NCDEX'];
-            let segToConsider = [ 'NSE', 'BSE', 'NFO'];
-            let finalTokenArr = data.data.filter(i=>{ return segToConsider.indexOf(i.exch_seg) != -1 })
+            let segToConsider = ['NSE', 'BSE', 'NFO'];
+            let finalTokenArr = data.data.filter(i => { return segToConsider.indexOf(i.exch_seg) != -1 })
             fs.writeFileSync(angelTokenPath, `module.exports = ${JSON.stringify(finalTokenArr)}`)
             console.log('Angel Broking Instruments Created Successfully!!')
         } catch (ex) {
@@ -30,19 +30,19 @@ axios({
 }, function (err) {
     console.log('Error while fetching data of Angel Broking Instruments!!')
 });
-function getTokenArray(list){
+function getTokenArray(list) {
     list = list.split('\n');
     list.pop()
     let headers = list[0];
     let keys = headers.split(',')
     let results = []
-    for(let i=1 ; i < list.length;i++){
+    for (let i = 1; i < list.length; i++) {
         let obj = {}
         let values = list[i].split(',');
-        for(let j=0;j<keys.length;j++){
-            if(j == 3){
-                obj[keys[j]] = String(values[j]).replace(/"/g,'');
-            }else{
+        for (let j = 0; j < keys.length; j++) {
+            if (j == 3) {
+                obj[keys[j]] = String(values[j]).replace(/"/g, '');
+            } else {
                 obj[keys[j]] = values[j];
             }
         }
@@ -62,8 +62,8 @@ axios({
         try {
             let tokenArr = getTokenArray(data.data)
             let allSeg = ['BCD-OPT', 'BCD-FUT', 'BCD', 'BSE', 'INDICES', 'CDS-OPT', 'CDS-FUT', 'MCX-FUT', 'MCX-OPT', 'NFO-OPT', 'NFO-FUT', 'NSE'];
-            let segToConsider = [ 'BSE', 'INDICES', 'NFO-OPT', 'NFO-FUT', 'NSE'];
-            let finalTokenArr = tokenArr.filter(i=>{ return segToConsider.indexOf(i.segment) != -1 })
+            let segToConsider = ['BSE', 'INDICES', 'NFO-OPT', 'NFO-FUT', 'NSE'];
+            let finalTokenArr = tokenArr.filter(i => { return segToConsider.indexOf(i.segment) != -1 })
             fs.writeFileSync(zerodhaTokenPath, `module.exports = ${JSON.stringify(finalTokenArr)}`)
             console.log('Zerodha Instruments Created Successfully!!')
         } catch (ex) {
